69d97aa7a0371034790ee702d08a8773cb5f8fc0
luizaoformigapqd <luizlima@garantti.com.br>
2023-09-13 11:45:40 -0300

    feat: Add intial structure for students

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Hubxp')
+    .setDescription('The Hubxp API description')
+    .setVersion('1.0')
+    .addTag('hubxp')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  if(process.env.ENVIRONMENT === "develop") {
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
